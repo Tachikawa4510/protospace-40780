da225520a0f3b96d1d2c302716703e6a5a85cc97
Tachikawa4510 <rstoyo3.tachikawa.hiromu@techcamp-training.com>
2024-05-30 15:38:23 +0900

    update6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user! ,only:[:show]
   # before_action  only: [:show]
   def show
-    @user = current_user
+    @user = User.find(params[:id])
     @prototypes = @user.prototypes
   end
 end
